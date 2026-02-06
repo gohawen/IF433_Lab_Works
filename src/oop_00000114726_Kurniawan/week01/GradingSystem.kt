2c6d90ca60588d6e1dbcd3bdc84113f2d2f153b1
gohawen <gohawen17@gmail.com>
2026-02-06 16:19:32 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000114726_Kurniawan/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000114726_Kurniawan.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
