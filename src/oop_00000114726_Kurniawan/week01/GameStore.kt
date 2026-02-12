1782bbc29a5b303d767cfba9300747a73da21168
gohawen <gohawen17@gmail.com>
2026-02-12 14:14:53 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_00000114726_Kurniawan/week01/GameStore.kt
@@ -0,0 +1,11 @@
+package oop_00000114726_Kurniawan.week01
+
+fun main() {
+
+    // Persiapan data awal
+    val gameTitle: String = "Minecraft"
+    val price: Int = 500000
+
+    println("Game Title : $gameTitle")
+    println("Price      : Rp $price")
+}
