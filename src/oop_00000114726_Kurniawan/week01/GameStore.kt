21ca4ac3358b7eaefe05c43a6ec74a3c63b7017b
gohawen <gohawen17@gmail.com>
2026-02-12 14:23:41 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000114726_Kurniawan/week01/GameStore.kt
+++ src/oop_00000114726_Kurniawan/week01/GameStore.kt
@@ -1,17 +1,34 @@
 package oop_00000114726_Kurniawan.week01
 
-// Fungsi logika diskon
 fun calculateDiscount(price: Int): Int =
     if (price > 500000)
         (price * 20) / 100
     else
         (price * 10) / 100
 
+fun printReceipt(title: String, finalPrice: Int) {
+
+    println("===== STRUK PEMBELIAN =====")
+    println("Game Title  : $title")
+    println("Harga Akhir : Rp $finalPrice")
+    println("============================")
+
+}
+
 fun main() {
+
     // Persiapan data awal
     val gameTitle: String = "Minecraft"
     val price: Int = 500000
 
-    println("Game Title : $gameTitle")
-    println("Price      : Rp $price")
+    // hitung diskon
+    val discount = calculateDiscount(price)
+
+    // harga akhir
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
 }
