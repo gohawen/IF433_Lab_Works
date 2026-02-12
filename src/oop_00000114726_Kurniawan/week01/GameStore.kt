bc09038bf5752e5f5e564a925dcad9237c7971ad
gohawen <gohawen17@gmail.com>
2026-02-12 14:20:00 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000114726_Kurniawan/week01/GameStore.kt
+++ src/oop_00000114726_Kurniawan/week01/GameStore.kt
@@ -1,7 +1,13 @@
 package oop_00000114726_Kurniawan.week01
 
-fun main() {
+// Fungsi logika diskon
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000)
+        (price * 20) / 100
+    else
+        (price * 10) / 100
 
+fun main() {
     // Persiapan data awal
     val gameTitle: String = "Minecraft"
     val price: Int = 500000
