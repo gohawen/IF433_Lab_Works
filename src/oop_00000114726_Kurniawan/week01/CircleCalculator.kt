72f3c69b8bf7b7a4dae224ed7f9a99ec0b32f8d0
gohawen <gohawen17@gmail.com>
2026-02-12 14:05:04 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_00000114726_Kurniawan/week01/CircleCalculator.kt
+++ src/oop_00000114726_Kurniawan/week01/CircleCalculator.kt
@@ -12,13 +12,9 @@ fun main(args: Array<String>) {
     println("Radius: $radius, Area: $area")
 
     // Logic Check
-    checkSize(area)
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+fun checkSize(area: Double): String =
+    if (area > 100) "This is a Big Circle"
+    else "This is a Small Circle"
