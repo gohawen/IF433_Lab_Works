c5db4d19894d6c576d1e817f832b930514e428d4
gohawen <gohawen17@gmail.com>
2026-02-20 15:10:22 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000114726_Kurniawan/week03/MainTask.kt
@@ -0,0 +1,11 @@
+package oop_00000114726_Kurniawan.week03
+
+fun main() {
+    val sword = Weapon("Excalibur")
+
+    sword.damage = -50
+
+    sword.damage = 9999
+
+    println("Tier Senjata: ${sword.tier}")
+}
