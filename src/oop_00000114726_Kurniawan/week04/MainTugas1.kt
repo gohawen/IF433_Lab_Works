57cc8b6fddb944cfe3bd9b721508949562719c28
gohawen <gohawen17@gmail.com>
2026-02-27 13:45:35 +0700

    week04: (task 1) test ElectricCar in main

---
+++ src/oop_00000114726_Kurniawan/week04/MainTugas1.kt
@@ -0,0 +1,10 @@
+package oop_00000114726_Kurniawan.week04
+
+fun main() {
+    println("\n--- Testing Electric Car ---")
+    val myTesla = ElectricCar("Tesla Model 3", 4, 85)
+
+    myTesla.accelerate()
+    myTesla.honk()
+    myTesla.openTrunk()
+}
