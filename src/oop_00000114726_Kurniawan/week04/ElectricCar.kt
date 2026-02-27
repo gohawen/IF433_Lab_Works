5f7b80ef28566e102a28d311e68fce62a7bb8337
gohawen <gohawen17@gmail.com>
2026-02-27 13:40:27 +0700

    week04: (task 1) create ElectricCar class with final override

---
+++ src/oop_00000114726_Kurniawan/week04/ElectricCar.kt
@@ -0,0 +1,11 @@
+package oop_00000114726_Kurniawan.week04
+
+class ElectricCar(
+    brand: String,
+    numberOfDoors: Int,
+    val batteryCapacity: Int
+) : Car(brand, numberOfDoors) {
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+}
