eafc19221a862cb28b41000e08bc86ad77583ebe
gohawen <gohawen17@gmail.com>
2026-03-13 14:55:21 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000114726_Kurniawan/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000114726_Kurniawan.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
