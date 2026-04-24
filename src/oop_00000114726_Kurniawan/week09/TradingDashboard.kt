b1925c2b5dea8b47e91baebb731b2e8506939588
gohawen <gohawen17@gmail.com>
2026-04-24 15:04:55 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000114726_Kurniawan/week09/TradingDashboard.kt
+++ src/oop_00000114726_Kurniawan/week09/TradingDashboard.kt
@@ -23,4 +23,7 @@ fun main() {
 
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
+
+    worstPerformersString.forEach { println(it) }
+    println("\nUnique Pairs Traded: $uniquePairs")
 }
