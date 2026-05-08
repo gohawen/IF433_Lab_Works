7da4065e9d5c0e15c04b8893e0c6c69cd89de06a
gohawen <gohawen17@gmail.com>
2026-05-08 09:14:18 +0700

    week10: (task) populate coin repository

--- src/oop_00000114726_Kurniawan/week10/CryptoDashboard.kt
+++ src/oop_00000114726_Kurniawan/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000114726_Kurniawan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 2.5))
+    coinRepo.add(Coin("ETH", 14.2))
+    coinRepo.add(Coin("USDT", 500.0))
 }
