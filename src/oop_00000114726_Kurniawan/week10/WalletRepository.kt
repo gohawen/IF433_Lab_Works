ba85123fc2dffc5849002f6a8865d9671198427c
gohawen <gohawen17@gmail.com>
2026-05-08 09:09:28 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000114726_Kurniawan/week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_00000114726_Kurniawan.week10
+
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
