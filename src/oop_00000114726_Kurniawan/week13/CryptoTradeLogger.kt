f7d3480afb981119138f7bd5876c415a0cca973e
gohawen <gohawen17@gmail.com>
2026-05-21 12:54:08 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000114726_Kurniawan/week13/CryptoTradeLogger.kt
@@ -0,0 +1,11 @@
+package oop_00000114726_Kurniawan.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
